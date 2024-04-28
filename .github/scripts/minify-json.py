ec151ea825be7516371423a06ad5838d5cdb7d1d
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2024-04-28 16:16:30 +0800

    Code formatting

--- .github/scripts/minify-json.py
+++ .github/scripts/minify-json.py
@@ -3,6 +3,7 @@ from json import dump, load
 from os.path import isfile
 from os import system
 
+
 def minify(name):
     in_path = f"{name}.json"
     out_path = f"{name}.min.json"
@@ -10,7 +11,7 @@ def minify(name):
     if not isfile(in_path):
         print(f"{in_path} is not a file!")
         return
-    
+
     content = None
     print(f"Reading file {in_path}...")
     with open(in_path, "r", encoding="utf-8") as f_in:
@@ -18,14 +19,22 @@ def minify(name):
 
     print(f"Writing file {out_path}...")
     with open(out_path, "w", encoding="utf-8") as f_out:
-        dump(content, f_out, ensure_ascii=False, check_circular=False, indent=None, separators=(",", ":"))
+        dump(
+            content,
+            f_out,
+            ensure_ascii=False,
+            check_circular=False,
+            indent=None,
+            separators=(",", ":"),
+        )
 
     print(f"Adding {out_path} to git...")
     system(f"git add {out_path}")
+
 
 if __name__ == "__main__":
     with open("./list.json", "r") as f:
         names = load(f)
         for name in names:
             minify(name)
-    print('Done! 🎉')
+    print("Done! 🎉")
--- .github/scripts/purge-jsdelivr.py
+++ .github/scripts/purge-jsdelivr.py
@@ -6,12 +6,16 @@ from time import sleep
 GH_REPO = "PRO-2684/pURLfy-rules"
 x = Session()
 
+
 def _purge(file):
     url = f"https://purge.jsdelivr.net/gh/{GH_REPO}@latest/{file}"
     print(f"Purging {url}...")
     r = x.get(url)
     if r.status_code != 200:
-        print(f"Failed to purge {url}! Status code: {r.status_code}, response: {r.text}")
+        print(
+            f"Failed to purge {url}! Status code: {r.status_code}, response: {r.text}"
+        )
+
 
 def purge(name):
     json_path = f"{name}.json"
@@ -20,6 +24,7 @@ def purge(name):
         _purge(json_path)
     if isfile(min_path):
         _purge(min_path)
+
 
 if __name__ == "__main__":
     with open("./list.json", "r") as f:
